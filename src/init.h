80a8f30c544b1462e48dff0efa837bbb84bbbdec
futtz <32785753+futtz@users.noreply.github.com>
2017-10-21 03:46:40 +0200

    Add files via upload

---
+++ src/init.h
@@ -0,0 +1,13 @@
+/*
+	by futtz @ github.com/futtz
+*/
+
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include "cheats.h"
+
+#define LOOKUP_DELAY 1250
+#define PROC_ID "gta_sa.exe"
+#define CONSOLE_ATTRIBUTE 13
+#define RELEASE_VERSION "Pre-Alpha"
